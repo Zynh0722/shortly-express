877397fdb7bd3517fac046ec7b8b33815b54953a
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-18 11:30:43 -0700

    First batch of tests passing

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const db = require('./db');
 const port = 4568;
 
 app.listen(port, () => {
-  console.log(`Shortly is listening on ${port}`);
+  console.log(`Shortly is listening on http://127.0.0.1:${port}/`);
 });
