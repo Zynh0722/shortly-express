d62e2b9d1fd9f979faa9dc4091d730f18709aa2d
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-19 10:21:42 -0700

    Add auth.createSession and pass some more tests

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const app = express();
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 app.use(cookieParser);
+app.use(Auth.createSession);
 app.use(partials());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,47 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
+const queryToSession = queryResult => (
+  {
+    id: queryResult.id,
+    hash: queryResult.hash,
+    userId: queryResult.userId,
+    user: queryResult.user
+  }
+);
+
+const createSession = (req, res, next) => {
+  models.Sessions.create()
+    .then(result => models.Sessions.get({id: result.insertId}))
+    .then(session => {
+      req.session = { id: session.id, hash: session.hash, userId: session.userId };
+      req.cookies.shortlyid = session.hash;
+      res.cookie('shortlyid', session.hash, { maxAge: (1000 * 60 * 60), path: '/' });
+      next();
+    });
+};
+
 module.exports.createSession = (req, res, next) => {
+  // res.cookie('shortlyid', 'heehoodd', { maxAge: (1000 * 60 * 60), path: '/' });
+  if (Object.keys(req.cookies).length === 0) {
+    createSession(req, res, next);
+  } else {
+    models.Sessions.get({hash: req.cookies.shortlyid})
+      .then(sessionQuery => {
+        if (sessionQuery) {
+          req.session = queryToSession(sessionQuery);
+          next();
+        } else {
+          createSession(req, res, next);
+        }
+      });
+  }
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
 
+module.exports.logIn = (req, res, next) => {
+
+};
